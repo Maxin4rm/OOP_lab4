a42ffe88dfcec0d86914ae6242f8e057fa0701fb
Maxin4rm <92393477+Maxin4rm@users.noreply.github.com>
2023-05-30 20:12:05 +0200

    Add files via upload

---
+++ plugins/GZFileArchiverPlugin/GZFileArchiver.cs
@@ -0,0 +1,43 @@
+﻿using System.IO.Compression;
+using System.IO;
+
+namespace GZipArchiverPlugin
+{
+    public class GZFileArchiver : WindowsFormsApp1.PluginInterface
+    {
+
+        public string FileExt => ".gz";
+
+        public void Archive(FileStream fileStream)
+        {                
+            fileStream.Seek(0, SeekOrigin.Begin);
+            using (MemoryStream compressedStream = new MemoryStream())
+            {
+                using (GZipStream gzipStream = new GZipStream(compressedStream, CompressionMode.Compress, true))
+                {
+                    fileStream.CopyTo(gzipStream);
+                }
+                fileStream.Position = 0;
+                fileStream.SetLength(compressedStream.Length);
+                compressedStream.Position = 0;
+                compressedStream.CopyTo(fileStream);
+            }
+        }
+
+
+        public void Dearchive(MemoryStream archiveStream)
+        {
+            archiveStream.Position = 0;
+            using (GZipStream gzip = new GZipStream(archiveStream, CompressionMode.Decompress, leaveOpen: true))
+            {
+                MemoryStream unpackedStream = new MemoryStream();
+                gzip.CopyTo(unpackedStream);
+                unpackedStream.Position = 0;
+                archiveStream.SetLength(unpackedStream.Length);
+                archiveStream.Position = 0;
+                unpackedStream.CopyTo(archiveStream);
+            }
+            archiveStream.Position = 0;   
+        }
+    }  
+}
---
+++ plugins/ZipFileArchiverPlugin/ZipFileArchiver.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.IO.Pipes;
+
+namespace ZipArchiverPlugin
+{
+    public class ZipFileArchiver : WindowsFormsApp1.PluginInterface
+    {
+        public string FileExt => ".zip";
+
+        public void Archive(FileStream fileStream)
+        {
+            string originalFile = Path.GetFileNameWithoutExtension(fileStream.Name);
+            fileStream.Position = 0;
+            using (ZipArchive archive = new ZipArchive(fileStream, ZipArchiveMode.Create))
+            {
+                ZipArchiveEntry entry = archive.CreateEntry(originalFile);
+                using (Stream entryStream = entry.Open())
+                {
+                    fileStream.CopyTo(entryStream);
+                }
+            }
+        }
+
+        
+
+        public void Dearchive(MemoryStream archiveStream)
+        {
+            using (ZipArchive archive = new ZipArchive(archiveStream, ZipArchiveMode.Read, true))
+            {
+                ZipArchiveEntry entry = archive.Entries[0];
+                using (Stream stream = entry.Open())
+                {
+                    archiveStream.Position = 0;
+                    archiveStream.SetLength(entry.Length);
+                    stream.CopyTo(archiveStream);
+                }
+                archiveStream.Position = 0;
+            }
+        }
+    }
+}
